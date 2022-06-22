c17c2fbf7840a3668347c19eefa1fc5249d4902a
Elliot Alderson <pgsilva0698@gmail.com>
2022-06-22 00:49:59 -0300

    refac

--- moovies/handler.js
+++ moovies/handler.js
@@ -5,5 +5,6 @@ const result = require('./domain/result')
 module.exports.search = async (event) => {
   console.log(`[INFO] ${Date(Date.now())} Lambda disparada `)
   console.log(`[INFO] Evento recebido: ${JSON.stringify(event)} `)
-  return await result.run(event);
-};
+ 
+  return await result.run(event)
+}
